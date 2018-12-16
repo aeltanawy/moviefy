9e2fe75441180c947a6a8871803b22cb9d2a7c33
Abeer Eltanawy <abeer.eltanawy@schrodinger.com>
2018-12-16 02:13:24 -0800

    removed unnecessary migration

--- db/migrate/20181215095056_migrateexpdate.rb
+++
@@ -1,9 +0,0 @@
-class Migrateexpdate < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :credit_cards, :exp_date
-    remove_column :billing_addresses, :exp_month
-    remove_column :billing_addresses, :exp_year
-    add_column :credit_cards, :exp_month, :integer
-    add_column :credit_cards, :exp_year, :integer
-  end
-end
--- db/migrate/20181216032735_create_credit_cards.rb
+++ db/migrate/20181216032735_create_credit_cards.rb
@@ -3,7 +3,8 @@ class CreateCreditCards < ActiveRecord::Migration[5.2]
     create_table :credit_cards do |t|
       t.string :name_on_card
       t.integer :card_num
-      t.date :exp_date
+      t.integer :exp_month
+      t.integer :exp_year
       t.integer :security_code
       t.references :user, foreign_key: true
 

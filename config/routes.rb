e8809812bc019c143925621cc86a80e84f0f9d1f
Abeer Eltanawy <abeer.eltanawy@schrodinger.com>
2018-12-15 18:51:32 -0800

    created scaffold users with new user form adjusted with correct password and email authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   resources :billing_addresses
   resources :credit_cards
   resources :purchasings
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_15_095056) do
+ActiveRecord::Schema.define(version: 2018_12_16_020437) do
 
   create_table "billing_addresses", force: :cascade do |t|
     t.string "street"
@@ -97,6 +97,18 @@ ActiveRecord::Schema.define(version: 2018_12_15_095056) do
     t.string "zipcode"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.date "dob"
+    t.string "gender"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
